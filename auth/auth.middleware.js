aaed3c3fa765b1e30aa7ad31e6037dc3365c67b2
Anonymous <>
2024-06-15 22:57:17 +0300

    Fix auth middleware response status code

--- auth/auth.middleware.js
+++ auth/auth.middleware.js
@@ -2,12 +2,12 @@ export default class AuthMiddleware {
   static validateTokenMiddleware(req, res, next) {
     // Check if 'token' exists in the request body
     if (!req.body || !req.body.token) {
-      return res.json({ error: 'Unauthorized: Missing auth token' });
+      return res.status(401).json({ error: 'Unauthorized: Missing auth token' });
     }
 
     // Compare the token with the expected value
     if (req.body.token !== process.env.SERVICE_TOKEN) {
-      return res.json({ error: 'Unauthorized: Invalid auth token' });
+      return res.status(403).json({ error: 'Unauthorized: Invalid auth token' });
     }
 
     // Valid token, continue processing the request (call next())
